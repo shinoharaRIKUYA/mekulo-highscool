e6fa00f133551222a7abc560c7a5df5eaccb90be
fukushi-mako <zhenzichengye@gmail.com>
2025-04-11 16:46:31 +0900

    0411

--- c/kouwa/!data.php
+++ c/kouwa/!data.php
@@ -1,13 +1,13 @@
 <?php
 $co = [
-    'slug' => 'sohfu',
+    'slug' => 'kouwa',
     'image' => 'company.jpg',
-    'company_name' => '株式会社創風',
-    'catch_copy' => '一枚の金属板から、快適な空間を作る',
-    'industry' => ['D'],
-    'post_code' => '386-2201',
-    'address' => '長野県上田市真田町長2411-12',
-    'homepage' => 'https://sohfu.co.jp/',
+    'company_name' => '交和物産株式会社',
+    'catch_copy' => '信州の味を、全国へ',
+    'industry' => ['E'],
+    'post_code' => '389-0603',
+    'address' => '長野県埴科郡坂城町大字南条5181-3 （テクノさかき工業団地）',
+    'homepage' => 'https://ajinokakehashi.com/',
     'logo_filename' => 'logo.svg',
     'latitude' => 36.3694332,//緯度
     'longitude' => 138.2142701,//経度
@@ -19,15 +19,15 @@ $co = [
 
 // 募集職種
 $occupation = [
-    'job_genba' => [
-        'name' => '現場作業員',
-        'url' => 'job_genba',
-        'catch_copy' => '県内にわずか200人！？ダクト職人になりませんか',
-        'work' => ['D'],
-        'fv_url' => '/job_genba/fv.jpg',
+    'job_seizou' => [
+        'name' => '食品製造職',
+        'url' => 'job_seizou',
+        'catch_copy' => 'おいしい、安心、うれしいを作る',
+        'work' => ['533'],
+        'fv_url' => 'introduce4.jpg',
         'latitude' => 36.3694332,//緯度
         'longitude' => 138.2142701,//経度    
-        'MBTI' =>  'infp',
+        'MBTI' =>  'isfj',
     ],
 ];
 
@@ -37,27 +37,27 @@ $visit = [
         'about' => '応募前見学',
         'date' => [
             [
-                'name' => '2025年7月29日（火） 10時30分～、13時30分～', 
+                'name' => '2025年7月29日（火） 10時00分～', 
                 'date' => '2025-07-29',
                 'deadline' => '2025-07-26',//募集締切日
             ],
             [
-                'name' => '2025年7月31日（木） 10時30分～、13時30分～', 
+                'name' => '2025年7月31日（木） 10時00分～', 
                 'date' => '2025-07-31',
                 'deadline' => '2025-07-28',//募集締切日
             ],
             [
-                'name' => '2025年8月7日（木） 10時30分～、13時30分～', 
-                'date' => '2025-08-07',
-                'deadline' => '2025-08-04',//募集締切日
+                'name' => '2025年8月6日（木） 10時00分～', 
+                'date' => '2025-08-06',
+                'deadline' => '2025-08-03',//募集締切日
             ],
             [
-                'name' => '2025年8月21日（木） 10時30分～、13時30分～', 
-                'date' => '2025-08-21',
-                'deadline' => '2025-08-18',//募集締切日
+                'name' => '2025年8月8日（木） 10時00分～', 
+                'date' => '2025-08-08',
+                'deadline' => '2025-08-05',//募集締切日
             ],
         ],
-        'catch_copy' => 'ダクト作りを覗いてみよう！',
+        'catch_copy' => 'チームワークを体感しよう！',
         'visit_img' =>'firstview.jpg',
         'content' => '応募前見学として参加する方は参加申し込みをしてください。',
     ],
