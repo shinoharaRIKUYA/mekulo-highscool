6809973919bca6ad03b52a25a69c47169066c7e6
fukushi-mako <zhenzichengye@gmail.com>
2025-06-11 18:03:02 +0900

    0611

---
+++ c/iijimashokuhin/!data.php
@@ -0,0 +1,45 @@
+<?php
+$co = [
+    'slug' => 'iijimashokuhin',
+    'image' => 'company.jpg',
+    'company_name' => '飯島食品株式会社',
+    'catch_copy' => '『なめ茸』一筋！ 全国へ、信州の魅力を届ける',
+    'industry' => ['E'],
+    'post_code' => '389-0822',
+    'address' => '長野県千曲市大字上山田417-1',
+    'homepage' => 'https://www.iijimashokuhin.com/',
+    'logo_filename' => 'logo.png',
+    'latitude' => 36.3694332,//緯度
+    'longitude' => 138.2142701,//経度
+    'human' => [
+        'image' => 'sato.png',
+        'name' => '佐藤',
+    ]
+];
+
+
+// 応募前見学
+$visit = [
+    [
+        'about' => '応募前見学',
+        'date' => [
+            [
+                'name' => '2025年7月29日（火） 10時00分～', 
+                'date' => '2025-07-29',
+                'deadline' => '2025-07-26',//募集締切日
+            ],
+        ],
+        'catch_copy' => '',
+        'visit_img' =>'firstview.jpg',
+        'content' => '応募前見学として参加する方は参加申し込みをしてください。',
+    ],
+];
+
+// 目次
+$toc = [
+    'top' => [
+        'name' => '企業TOP',
+        'icon' => 'company.png',
+        'url' => '',
+    ],
+	];
---
+++ c/iijimashokuhin/index.php
@@ -0,0 +1,508 @@
+<?php
+$base = '../../../';
+$highschool_base = '../../';
+$company_base = './';
+$cur_page = 'top';
+include_once $company_base . '!data.php';
+include_once $highschool_base . 'circle.php';
+include_once $highschool_base . '!master-data.php';
+
+//====================
+// ページのメタデータ
+$title = $co['company_name'] . 'の新卒求人情報';
+$title_with_site = $title . ' | めくろうワークス';
+$keywords = '';//メタキーワードの追加
+$description = '';//メタディスクリプションの追加
+
+$canonical_url = 'https://works.mekulo.jp/highschool/' . $co['slug'] . '/';
+
+$og_image = 'https://works.mekulo.jp/ogp-image.png';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<?php //include $base . '../tracking.php'; ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?= $highschool_base ?>highschool.css?v=2">
+<link rel="icon" href="<?= $base ?>mekulo-works-mark.png">
+<link rel="canonical" href="<?= $canonical_url ?>">
+<title><?= $title_with_site ?></title>
+<?php if ($description) { ?>
+<meta name="description" content="<?= $description ?>">
+<?php } ?>
+<?php if ($keywords) { ?>
+<meta name="keywords" content="<?= $keywords ?>">
+<?php } ?>
+<meta property="og:type" content="article">
+<meta property="og:title" content="<?= $title_with_site ?>">
+<?php if ($description) { ?>
+<meta property="og:description" content="<?= $description ?>">
+<?php } ?>
+<meta property="og:url" content="<?= $canonical_url ?>">
+<meta property="og:image" content="<?= $og_image ?>">
+<meta property="fb:app_id" content="534360210586966">
+<script type="application/ld+json">
+{
+	"@context": "https://schema.org",
+	"@type": "BreadcrumbList",
+	"itemListElement": [{
+		"@type": "ListItem",
+		"position": 1,
+		"name": "めくろうワークス",
+		"item": "https://works.mekulo.jp/"
+	},{
+		"@type": "ListItem",
+		"position": 2,
+		"name": "高卒特集",
+		"item": "https://works.mekulo.jp/highschool/"
+	},{
+		"@type": "ListItem",
+		"position": 3,
+		"name": "<?= $co['company_name'] ?>",
+		"item": "https://works.mekulo.jp/highschool/<?= $co['slug'] ?>/"
+	}]
+}
+</script>
+
+</head>
+<body class="highschool">
+<?php include $highschool_base . 'tmpl_header.php'; ?>
+<div class="content_area">
+<main>
+<div class="fv_area">
+	<img alt="" class="fv_image" src="firstview.jpg">
+	<hgroup>
+		<h1 class="catch"><?= $co['catch_copy'] ?></h1>
+		<p class="fv-tag"><?= INDUSTRIES[$co['industry'][0]] ?></p>
+	</hgroup>
+</div>
+<section class="toc">
+	<hgroup>
+			<h2>目次</h2>
+			<div class="click">
+				<span></span>
+				<span></span>
+			</div>
+	</hgroup>
+	<ol>
+		<li class="abu"><a href="#about_company">会社紹介記事</a></li>
+		<li class="pro"><a href="#business">私たちのビジネス</a></li>
+		<li class="num-d"><a href="#numeric_data">数字で見る当社</a></li>
+		<li class="f-pre"><a href="#from_president">社長メッセージ</a></li>
+		<li class="com-in"><a href="#company_info">会社概要</a></li>
+	</ol>
+</section>
+<div class="about-com content" id="company_gallery">
+	<h2>スーパーに並んでいる『なめ茸』、作っています！</h2>
+	<p>
+		スーパーで売っている、瓶に入った「なめ茸」。<br>
+		それを“生のえのき茸”から加工して商品にしているのが、私たち飯島食品です。地元の農家さんと協力して、毎日800kg以上のえのきを全国へ届けています。<br>
+		農家さんから届いた新鮮なえのきを選別し、煮込んで、ビンに詰める。私たちがいつも何気なく食べている食品の裏側には、地域に根差した丁寧な加工現場があります。ぜひ一度、見学に来てください！
+	</p>
+	<div id="gallery">
+		<div class="gallery_window">
+			<img src="first.jpg" alt="" class="fs show">
+			<img src="second.jpg" alt="" class="sc">
+			<img src="third.jpg" alt="" class="th">
+			<img src="fourth.jpg" alt="" class="fo">
+		</div>
+		<div class="gallery_index">
+			<img src="first.jpg" alt="" class="fs">
+			<img src="second.jpg" alt="" class="sc">
+			<img src="third.jpg" alt="" class="th">
+			<img src="fourth.jpg" alt="" class="fo">
+		</div>
+	</div>
+	<h3>取材担当者からのポイント！</h3>
+	<h4 class="point">「真面目にやりたい」気持ちが活かせる職場</h4>
+	<figure class="portrait" style="width: 20%;">
+		<img src=<?= $highschool_base . WRITER['sato']['image'] //担当者名をここに記入?> alt="">
+		<figcaption><span class="name"><?= WRITER['sato']['name']//担当者名をここに記入 ?></span></figcaption>
+	</figure>
+	<p style="min-height: 160px;">
+		工場内を見学してまず感じたのは、社員さんの気持ちの良い挨拶でした。こちらから声をかける前に挨拶してくださる方が多く、職場全体の雰囲気の良さが伝わってきました。<br>
+		食品を扱う会社だからこそ、衛生管理は徹底されています。帽子の中に髪の毛をきちんと入れ、手洗いや消毒もしっかり行なうなど、「当たり前のことを当たり前にやる」姿勢が印象的でした。<br>
+		どの作業工程でも、みなさんがテキパキと動いていて、仕事中は集中、休憩中はリラックス、とオンオフのメリハリがしっかりしています。<br>
+		学校生活の中で、「掃除は真面目にやってほしい」「授業中は静かにしてほしい」と思ったことがある人にこそ、特に向いている職場だと思います。そんなあなたなら、きっと飯島食品で心地よく働けるでしょう。
+	</p>
+</div>
+<section class="content" id="about_company">
+	<h2>会社情報記事</h2>
+	<h3>
+		新鮮なえのきを、おいしく加工する
+	</h3>
+	<p>
+		飯島食品の主な仕事は、“えのき茸”を使った加工食品の製造です。<br>
+		ビンに入った「なめ茸」や「うめなめ茸」など、さまざまな味の総菜を作っています。
+	</p>
+	<div class="images">
+		<img src="introduce1.jpg" class="scene" alt="">
+	</div>
+	<p>
+		毎朝8時に、地元の農家さんから新鮮なえのきが届きます。これは、農家さんが2か月以上かけて育ててくれた大切な原料です。
+	</p>
+	<div class="images">
+		<img src="introduce2.jpg" class="scene" alt="">
+	</div>
+	<p>
+		届いたえのきは1～2センチにカットされていて、1日でおよそ800kg分を加工します。製造の中心は「煮込み」「充填（ビン詰め）」「密封（フタ閉め）」の3つの工程で、製造スタッフ全員がそれぞれの役割を担っています。
+	</p>
+	<div class="images">
+		<img src="introduce3.jpg" class="scene" alt="">
+	</div>
+	<h3>お客様の目線で、異物を取り除く</h3>
+	<p>
+		製造前の「選別」で一番大切なのは、「お客様だったらどう感じるか」という気持ちです。異物が少しでも混ざっていたら、たとえ体に害がなくても気になってしまいますよね。<br>
+		そこで、えのきの石づきなどの固い部分、たまに混ざるビニール片や髪の毛などを、手作業で丁寧に取り除いていきます。毎朝8時からお昼くらいまで、6～7人で集中して行なっています。
+	</p>
+	<div class="images">
+		<img src="first.jpg" class="scene" alt="">
+		<img src="introduce4.jpg" class="scene" alt="">
+		<img src="introduce5.jpg" class="scene" alt="">
+	</div>
+	<p>
+		入社後に初めて教わるのも、この選別工程。細かい作業ですが、「丁寧に」「正確に」を意識することで、食品づくりの基本がしっかり身につきます。
+	</p>
+	<h3>動きがあるから飽きない</h3>
+	<p>
+		選別工程を学んだあとは、実際の製造工程に挑戦していきます。<br>
+		飯島食品では、一つの工程だけをやり続けることはありません。その日その時でさまざまなポジションに入ります。
+	</p>
+	<div class="images">
+		<img src="second.jpg" class="scene" alt="">
+		<img src="third.jpg" class="scene" alt="">
+		<img src="introduce8.jpg" class="scene" alt="">
+	</div>
+	<p>
+		製造工程では、14人ほどが働いています。リーダーの指示のもと、みんなで協力しながら製造していきます。
+	</p>
+	<p>
+		『煮込み』<br>
+		選別されたえのきに調味料を加え、大きな釜でじっくり煮込みます。味をしっかり染み込ませるために、火加減や時間の管理が大切な作業です。
+	</p>
+	<div class="images">
+		<img src="introduce9.jpg" class="scene" alt="">
+	</div>
+	<p>
+		『充填（瓶詰め）』<br>
+		ビンのサイズや量に合わせて機械を調整し、リズムよく中身を詰めていきます。ここでは、スピードと正確さが求められます。
+	</p>
+	<div class="images">
+		<img src="introduce10.jpg" class="scene" alt="">
+		<img src="introduce11.jpg" class="scene" alt="">
+	</div>
+	<p>
+		『密封（フタ閉め）』<br>
+		機械を使って、ビンのふたをしっかり閉めていきます。最後には人の目と手で、一つひとつ丁寧に確認することも欠かせません。
+	</p>
+	<div class="images">
+		<img src="introduce12.jpg" class="scene" alt="">
+		<img src="introduce8.jpg" class="scene" alt="">
+		<img src="introduce14.jpg" class="scene" alt="">
+		<img src="introduce15.jpg" class="scene" alt="">
+	</div>
+	<p>
+		それぞれの工程がつながって、やっと一つの商品が完成します。どこか一つでも欠けたら成り立たない、チームワークが命の仕事です。<br>
+		入社後1か月ほどで基本的な流れを体験したら、各工程の作業のコツやノウハウを、先輩が隣で丁寧に教えてくれます。3か月ほどで、一通りの仕事が理解できるでしょう。
+	</p>
+	<div class="images">
+		<img src="introduce16.jpg" class="scene" alt="">
+		<img src="introduce17.jpg" class="scene" alt="">
+	</div>
+	<p>
+		仕事中はチームワークを大切にしていますが、休憩時間はそれぞれ自由に過ごしています。同じくらいの年齢の人とおしゃべりを楽しむ人もいれば、一人でゆったり過ごす人も。<br>
+		大切なのは、わからないことを素直に「わかりません」と言えること。「忘れちゃいました！」と正直に伝えられる人が歓迎されます。素直さと謙虚さが、仕事を覚える近道なんです。
+	</p>
+	<h3>めざせリーダー！全体を見渡せる存在に</h3>
+	<p>
+		将来的には、製造現場の「リーダー」を目指すこともできます。<br>
+		リーダーは、前日までに製造計画を立てて、その日の製造の順番や人の配置を決め、当日みんなに指示を出すポジションです。<br>
+		一つの製品を作るためには、量や時間、順序などをしっかり考えて進めていく必要があります。だからこそ、全体の流れを把握し、現場をうまく回していく力が求められます。
+	</p>
+	<div class="images">
+		<img src="introduce3.jpg" class="scene" alt="">
+		<img src="introduce18.jpg" class="scene" alt="">
+		<img src="fourth.jpg" class="scene" alt="">
+	</div>
+	<p>
+		リーダーになるには、少なくとも10年は必要。それだけ責任ある立場ですが、信頼される存在として、現場の中心で活躍できます。<br>
+		食品に関わる仕事をしてみたいあなたを、私たちは心から歓迎します。一歩ずつ、一緒に成長していきましょう！
+	</p>
+</section>
+<section id="business" class="content">
+	<h2>私たちのビジネス</h2>
+	<ul class="Lightbox">
+		<li class="thumbnails">
+			<div class="thumbnail">
+				<div class="inner">
+					<img src="business1.jpg" alt="">
+				</div>
+				<hgroup>
+					<h3>なめ茸</h3>
+					<p>
+						売れ筋ランキング1位。大根おろしや納豆をはじめ、さまざまな料理のトッピングにぴったりです。
+					</p>
+				</hgroup>
+			</div>
+			<p>なめ茸</p>
+		</li>
+		<li class="thumbnails">
+			<div class="thumbnail">
+				<div class="inner">
+					<img src="business2.jpg" alt="">
+				</div>
+				<hgroup>
+					<h3>うめなめたけ</h3>
+					<p>
+						売れ筋ランキング2位。えのき茸のシャキシャキ感と梅の酸味が絶妙。
+					</p>
+				</hgroup>
+			</div>
+			<p>うめなめたけ</p>
+		</li>
+		<li class="thumbnails">
+			<div class="thumbnail">
+				<div class="inner">
+					<img src="business3.jpg" alt="">
+				</div>
+				<hgroup>
+					<h3>赤唐味噌なめたけ</h3>
+					<p>
+						売れ筋ランキング3位。信州味噌の深い味わいと、赤唐辛子のピリ辛が食欲をそそります。
+					</p>
+				</hgroup>
+			</div>
+			<p>赤唐味噌なめたけ</p>
+		</li>
+		<li class="thumbnails">
+			<div class="thumbnail">
+				<div class="inner">
+					<img src="business4.jpg" alt="">
+				</div>
+				<hgroup>
+					<h3>きのこ三昧</h3>
+					<p>
+						きのこ（えのき茸、しいたけ）と山菜（わらび、きくらげ）のミックスを、しょうゆベースのたれで味付けしたもの。
+					</p>
+				</hgroup>
+			</div>
+			<p>きのこ三昧</p>
+		</li>
+	</ul>
+</section>
+<section class="content" id="numeric_data">
+	<h2>数字で見る当社</h2>
+	<p class="remark">（2025年2月20日現在）</p>
+	<div class="large">
+		<p>売上高</p>
+		<p class="num"><span>7.3</span>億円</p>
+		<p class="year">※2024年実績</p>
+	</div>
+	<ul class="gr_con">
+		<li class="small">
+			<p>平均年齢</p>
+			<p class="num"><span>45</span>歳</p>
+		</li>
+		<li class="small">
+			<p>平均勤続年数</p>
+			<p class="num"><span>10</span>年</p>
+		</li>
+		<li class="small">
+			<p>平均残業時間</p>
+			<p class="num"><span>14</span>時間</p>
+		</li>
+		<li class="small">
+			<p>年間休日</p>
+			<p class="num"><span>104</span>日</p>
+		</li>
+		<li class="small">
+			<p>設立年</p>
+			<p class="num"><span>1967</span>年</p>
+		</li>
+		<li class="small">
+			<p>平均有給取得日数</p>
+			<p class="num"><span>12</span>日</p>
+		</li>
+	</ul>
+	<ul class="fl_con">
+	<li>
+			<div class="chart">
+				<p>育児休暇率</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 45, [
+						[ 'value' => 20, 'caption' => '20%', 'color' => '#2378C7'],
+						[ 'value' => 80, 'caption' => '', 'color' => '#d9d9d9'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+			<div class="chart">
+				<p>新卒・中途の比率</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 45, [
+						[ 'value' => 3, 'caption' => '新卒3', 'color' => '#2378C7'],
+						[ 'value' => 7, 'caption' => '中途7', 'color' => '#F34444'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+			<div class="chart">
+				<p>男女比</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 40, [
+						[ 'value' => 28, 'caption' => '男性：28', 'color' => '#0062B5'],
+						[ 'value' => 15, 'caption' => '女性：15', 'color' => '#FF6E7E'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+		<div class="chart">
+			<p>年齢構成</p>
+			<svg width="150" height="150" viewBox="0 0 200 200">
+				<?php create_donut_graph(100, 100, 100, 40, [
+					[ 'value' => 0.001, 'caption' => "", 'color' => '#808080'],
+					[ 'value' => 7, 'caption' => '20代', 'color' => '#0073B7'],
+					[ 'value' => 3, 'caption' => '30代', 'color' => '#00AEEF'],
+					[ 'value' => 11, 'caption' => '40代', 'color' => '#00B398'],
+					[ 'value' => 11, 'caption' => '50代', 'color' => '#009F6B'],
+					[ 'value' => 11, 'caption' => '60代', 'color' => '#008C4A'],
+				]); ?>
+			</svg>
+		</div>
+		<li>
+			<div class="chart" >
+				<p>通勤手段の割合</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 40, [
+						[ 'value' => 41, 'caption' => "車・バイク", 'color' => '#E06A3A'],
+						[ 'value' => 2, 'caption' => '電車・バス', 'color' => '#D04255'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+			<div class="chart">
+				<p>出身地の割合</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 40, [
+						[ 'value' => 100, 'caption' => "長野県内", 'color' => '#B2CF3E'],
+						[ 'value' => 0.001, 'caption' => '', 'color' => '#FEE929'],
+												]); ?>
+				</svg>
+			</div>
+		</li>
+	</ul>	
+</section>
+<section class="content" id="company_info">
+	<h2>会社概要</h2>
+	<img src="company.jpg" alt="" class="scene">
+	<dl class="speclist">
+		<div>
+			<dt>会社名</dt>
+			<dd><?= $co['company_name'] ?></dd>
+		</div>
+		<div>
+			<dt>業種</dt>
+			<dd><?= INDUSTRIES[$co['industry'][0]] ?></dd>
+		</div>
+		<div>
+			<dt>事業内容</dt>
+			<dd>
+				<ul class="li_style">
+					<li>
+						
+					</li>
+				</ul>
+			</dd>
+		</div>
+		<div>
+			<dt>代表者名</dt>
+			<dd>平林　忍</dd>
+		</div>
+		<div>
+			<dt>従業員数</dt>
+			<dd>名</dd>
+		</div>
+		<div>
+			<dt>本社所在地</dt>
+			<dd>〒<?= $co['post_code']?><br><?= $co['address']?><br>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1183.2512533256145!2d138.15532027877967!3d36.468342852470975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d9eaa29f3a30d%3A0x633875bd2ae9de82!2z6aOv5bO26aOf5ZOBKOagqik!5e0!3m2!1sja!2sjp!4v1749629696945!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</dd>
+		</div>
+		<div>
+			<dt>本社電話番号</dt>
+			<dd>026-276-0011</dd>
+		</div>
+		<div class="hp">
+			<dt>ホームページ</dt>
+			<dd><a href="https://www.iijimashokuhin.com/">https://www.iijimashokuhin.com/</a></dd>
+		</div>
+	</dl>
+</section>
+<section class="content" id="from_president">
+	<h2>会社からのメッセージ</h2>
+	<div class="pre_con">
+		<figure class="portrait">
+			<img src="president_p.jpg" alt="">
+			<figcaption><span class="name"></span></figcaption>
+		</figure>
+		<p>
+			私たちが求めているのは、ルールを守れて、素直な人。食品を扱う仕事だからこそ、髪の毛を帽子にきちんと入れる、工場へ入る前には手をしっかり洗う、そういった基本的なルールを守ることが何より大切です。<br>
+			また、もし間違えてしまったときは、「間違えました」と伝えられる素直さも大切です。食品会社は、一度の失敗で信頼を失ってしまうこともあります。だからこそ、工場の中でミスの気付けたほうが良いのです。失敗しても、次から気を付ければ大丈夫。やさしくフォローしてくれる先輩たちがそばにいます。<br>
+			さらに、目標を持って前向きに挑戦できる人も大歓迎です。将来的には、リーダーとして活躍する道もあります。<br>
+			少しでも興味を持っていただけた方は、ぜひ一度、見学に来てみてください。お待ちしています。
+		</p>
+	</div>
+</section>
+<div class="visit content">
+    <h2>応募前見学について</h2>
+		<dl class="speclist">
+    <?php foreach ($visit as $plan): ?>
+    <div class="visit-item">
+        <h3><?= $plan['about'] ?>について</h3>
+        <div>
+            <dt>開催日</dt>
+            <dd>
+				<ul>
+						<?php 
+						if (is_array($plan['date'])) {
+								foreach ($plan['date'] as $date) {
+										if (is_array($date)) { 
+											echo "<li>" . $date['name'] . "</li>";
+										}
+								}
+						} else {
+								echo "<li>" . $plan['date'] . "</li>";
+						}
+						?>
+				</ul>
+            </dd>
+        </div>
+        <div>
+            <dt>内容</dt>
+            <dd><?= $plan['content'] ?></dd>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</dl>
+	<a href="apply/#entry" class="flag_btn_entry">
+		<img src="<?= $highschool_base ?>flag.png" class="flag">
+		<span>
+			就活前に会社を見てみよう!<br>
+			応募前見学
+		</span>
+		<img src="<?= $highschool_base ?>arrow.svg">
+	</a>
+</div>
+</main>
+<?php include $highschool_base . 'tmpl_navi.php' ?>
+</div>
+<script src="../../tab.js"></script>
+<script src="../../page_nav.js"></script>
+<script src="<?= $highschool_base ?>lightbox.js"></script>
+<?php //include $base . '../footer.php'; ?>
+</body>
