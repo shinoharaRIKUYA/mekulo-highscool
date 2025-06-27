7caa6ea337d58df43430f491ef0c309fa2dbd72d
KouzuAyumi9 <kouzu@nextreservation.jp>
2025-06-27 09:30:08 +0900

    250616 (#52)
    Co-authored-by: NaritaShogo05 <152235188+NaritaShogo05@users.noreply.github.com>

---
+++ c/furea-original/!data.php
@@ -0,0 +1,70 @@
+<?php
+$co = [
+    'slug' => 'furea-original',
+    'image' => 'factory.jpg',
+    'company_name' => '株式会社 フレアオリジナル',
+    'catch_copy' => '坂城町から世界を目指す！次世代を担うロボットメーカー',
+    'industry' => ['G'],
+    'post_code' => '389-0601',
+    'address' => '長野県埴科郡坂城町坂城9439-5',
+    'homepage' => 'https://www.flareoriginal.com/',
+    'logo_filename' => 'flarelogo.svg',
+    'latitude' => 36.4558151,//緯度
+    'longitude' => 138.1860619,//経度
+    'human' => [
+        'image' => 'shomura.png',
+        'name' => '正村 欣生',
+    ]
+];
+
+// 募集職種
+$occupation = [
+    'job_robot' => [
+        'name' => 'ロボットクリエーター',
+        'url' => 'job_robot',
+        'catch_copy' => '考えて、作って、動かす。ロボットに使命を与える仕事',
+        'work' => ['541','73'],
+        'fv_url' => 'job_robot/firstview.jpg',
+        'latitude' => 36.4558151,//緯度
+        'longitude' => 138.1860619,//経度    
+        'MBTI' => 'Int-', 
+    ],
+];
+
+// 応募前見学
+$visit = [
+    [
+        'about' => '応募前見学',
+        'date' => [
+            [
+                'name' => '2025年  月  日（ ）  時  分～', 
+                'date' => '2025-0 -  ',
+                'deadline' => '2025-0 -  ',//募集締切日
+            ],
+            [
+                'name' => '2025年 月 日（ ）  時  分～', 
+                'date' => '2025-0 -0 ',
+                'deadline' => '2025-0 -  ',//募集締切日
+            ],
+        ],
+        'catch_copy' => '',
+        'visit_img' =>'first-view.jpg',
+        'content' => '工場内をご案内しながら、作業内容について詳しくご説明致します。',
+    ],
+    
+];
+
+// 目次
+$toc = [
+    'top' => [
+        'name' => '企業TOP',
+        'icon' => 'company.png',
+        'url' => '',
+    ],
+    'job' => [
+        'name' => '募集職種',
+        'icon' => 'carrier.png',
+        'url' => '',
+        'submenu' => $occupation,
+		],
+	];
