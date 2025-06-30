a5017424680de3172db3469d77b0124598b05d9b
KouzuAyumi9 <kouzu@nextreservation.jp>
2025-06-30 19:02:09 +0900

    0630araisetsubi (#74)
    コーディング

---
+++ c/arai-setsubi/!data.php
@@ -0,0 +1,49 @@
+<?php
+$co = [
+    'slug' => 'arai-setsubi',
+    'image' => 'company.jpg',
+    'company_name' => '株式会社 荒井設備',
+    'catch_copy' => '見えない場所に、自分の仕事を残す',
+    'industry' => ['D'],
+    'post_code' => '389-0505',
+    'address' => '長野県東御市和1818-3',
+    'homepage' => 'https://arai-facility621615.storeinfo.jp/',
+    'logo_filename' => 'logo_a.png',
+    'latitude' => 36.370437,//緯度
+    'longitude' => 138.311116,//経度
+    'human' => [
+        'image' => 'sato.png',
+        'name' => '佐藤',
+    ]
+];
+
+// 応募前見学
+$visit = [
+    [
+        'about' => '応募前見学',
+        'date' => [
+            [
+                'name' => '2025年 月  日（ ）  時  分～', 
+                'date' => '2025-0 -  ',
+                'deadline' => '2025-0 -  ',//募集締切日
+            ],
+            [
+                'name' => '2025年 月 日（ ）  時  分～', 
+                'date' => '2025-0 -  ',
+                'deadline' => '2025-0 -  ',//募集締切日
+            ],
+        ],
+        'catch_copy' => '',
+        'visit_img' =>'first_view.jpg',
+        'content' => '',
+    ],
+];
+
+// 目次
+$toc = [
+    'top' => [
+        'name' => '企業TOP',
+        'icon' => 'company.png',
+        'url' => '',
+    ],
+	];
---
+++ c/arai-setsubi/index.php
@@ -0,0 +1,400 @@
+<?php
+$base = '../../../';
+$highschool_base = '../../';
+$company_base = './';
+$cur_page = 'top';
+include_once $company_base . '!data.php';
+include_once $highschool_base . 'circle.php';
+include_once $highschool_base . '!master-data.php';
+
+//====================
+// ページのメタデータ
+$title = $co['company_name'] . '株式会社 荒井設備の高卒求人情報';
+$title_with_site = $title . ' | めくろうワークス';
+$description = 'このページは株式会社荒井設備の企業紹介ページです。荒井設備は、給排水管やエアコン、換気扇など住宅設備の配管工事を中心に手掛けています。現場はチーム制で、多様な状況に対応しながら暮らしの基盤を支える仕事です！';//メタディスクリプションの追加 
+$canonical_url = 'https://works.mekulo.jp/hs/' . $co['slug'] . '/';
+$og_image = 'https://works.mekulo.jp/ogp-image.png';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<?php //include $base . '../tracking.php'; ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?= $highschool_base ?>highschool.css?v=2">
+<link rel="icon" href="<?= $base ?>mekulo-works-mark.png">
+<link rel="canonical" href="<?= $canonical_url ?>">
+<title><?= $title_with_site ?></title>
+<?php if ($description) { ?>
+<meta name="description" content="<?= $description ?>">
+<?php } ?>
+<meta property="og:type" content="article">
+<meta property="og:title" content="<?= $title_with_site ?>">
+<?php if ($description) { ?>
+<meta property="og:description" content="<?= $description ?>">
+<?php } ?>
+<meta property="og:url" content="<?= $canonical_url ?>">
+<meta property="og:image" content="<?= $og_image ?>">
+<meta property="fb:app_id" content="534360210586966">
+<script type="application/ld+json">
+{
+	"@context": "https://schema.org",
+	"@type": "BreadcrumbList",
+	"itemListElement": [{
+		"@type": "ListItem",
+		"position": 1,
+		"name": "めくろうワークス",
+		"item": "https://works.mekulo.jp/"
+	},{
+		"@type": "ListItem",
+		"position": 2,
+		"name": "高卒求人特集",
+		"item": "https://works.mekulo.jp/hs/"
+	},{
+		"@type": "ListItem",
+		"position": 3,
+		"name": "<?= $co['company_name'] ?>",
+		"item": "https://works.mekulo.jp/hs/<?= $co['slug'] ?>/"
+	}]
+}
+</script>
+
+</head>
+<body class="highschool">
+<?php include $highschool_base . 'tmpl_header.php'; ?>
+<div class="content_area">
+<main>
+<div class="fv_area">
+	<img alt="" class="fv_image" src="first_view.jpg">
+	<hgroup>
+		<h1 class="catch"><?= $co['catch_copy'] ?></h1>
+		<p class="fv-tag"><?= INDUSTRIES[$co['industry'][0]] ?></p>
+	</hgroup>
+</div>
+<section class="toc">
+	<hgroup>
+			<h2>目次</h2>
+			<div class="click">
+				<span></span>
+				<span></span>
+			</div>
+	</hgroup>
+	<ol>
+		<li class="abu"><a href="#about_company">会社紹介記事</a></li>
+		<li class="pro"><a href="#business">私たちのビジネス</a></li>
+		<li class="num-d"><a href="#numeric_data">数字で見る当社</a></li>
+		<li class="f-pre"><a href="#from_president">社長メッセージ</a></li>
+		<li class="com-in"><a href="#company_info">会社概要</a></li>
+	</ol>
+</section>
+<div class="about-com content" id="company_gallery">
+	<h2>
+		目立たないけど、必要な仕事
+	</h2>
+	<p>
+		荒井設備の仕事は、配管工事をはじめとする「暮らしの土台づくり」。<br>
+        給水管や排水管、エアコン、換気扇――私たちが取り付ける設備は、どれもみなさんの生活に欠かせないものばかりです。<br>
+        でも、ただ「便利な生活を支える」だけではありません。<br>
+        私たちが仕事をしているのは、家の壁の裏や地面の下、天井の奥。普段は目に見えない場所に、自分の手で作ったものが静かに残っていくんです。
+        それってなんだかワクワクしませんか？
+	</p>
+	<div id="gallery">
+		<div class="gallery_window">
+			<img src="gallery01.jpg" alt="" class="fs show">
+			<img src="gallery02.jpg" alt="" class="sc">
+			<img src="contents03.jpg" alt="" class="th">
+			<img src="gallery04.jpg" alt="" class="fo">
+		</div>
+		<div class="gallery_index">
+			<img src="gallery01.jpg" alt="" class="fs">
+			<img src="gallery02.jpg" alt="" class="sc">
+			<img src="contents03.jpg" alt="" class="th">
+			<img src="gallery04.jpg" alt="" class="fo">
+		</div>
+	</div>
+	
+</div>
+<section class="content" id="about_company">
+	<h2>会社情報記事</h2>
+	<h3>
+		暮らしを支える
+	</h3>
+	<p>
+		荒井設備では、給排水工事といった配管工事を中心に、エアコンや換気扇といった住宅設備の取り付けなどを行なっています。<br>
+		一般住宅はもちろん、長野県や東御市といった地方自治体から発注される公共工事にも携わっています。
+	</p>
+	<div class="images grid-img">
+		<img src="contents01.jpg" class="scene" alt="">
+		<img src="contents02.jpg" class="scene" alt="">
+		<img src="contents03.jpg" class="scene" alt="">
+	</div>
+	<p>
+		なかでも一番多いのが、「本管工事」と呼ばれる配管工事。<br>
+		重機や工具を使って、給水管や水道管の引き込みを行ないます。
+	</p>
+	<div class="images">
+		<img src="contents04.jpg" class="scene" alt="">
+	</div>
+	<p>
+		現場は2～6人のチーム制。<br>
+        いきなり工事をするわけではなく、通行止めの有無やガス管など他の配管が埋まっていないかの現地調査から始まります。地域住民への周知も、大事な仕事のひとつです。<br>
+    </p><br>
+	<p>
+		重機が入れないような狭い現場では、手作業で地面を掘ることもあります。
+        ときには、現場で溶接が必要になることも。
+    </p><br>
+	<p>
+		その場の状況を見て判断することが多いので、決まったことをこなすより、いろんなことをやりたい人に向いている仕事です。
+	</p>
+	<h3>
+		たとえば、こんな現場がある
+	</h3>
+	<p>
+		・6年かけて完成させた、市営住宅の水道管工事。<br>
+        ・築40年以上の団地の、洗面台や浴槽などのリフォーム工事。
+	</p><br>
+    <p>
+		毎日違う現場で、毎回違う景色が待っています。
+	</p>
+	<div class="images">
+		<img src="contents05.jpg" class="scene" alt="">
+		<img src="contents06.jpg" class="scene" alt="">
+	</div>
+	<p>
+		掘って、通して、つなぐ――なんて、簡単にはいきません。<br>
+        現場での作業は、図面通りにいかないことばかり。曲がった管、固い地面、予想外の障害物。<br>
+		そんなときこそ、「どうするか」を考えるのが、この仕事の面白さです。
+	</p>
+    <h3>
+		経験０でも大丈夫！
+	</h3>
+	<p>
+		最初は先輩と一緒に現場に出て、道具の名前や使い方、仕事の流れをひとつずつ覚えていきます。<br>
+        作業を見て、やってみて、わからなければ質問する。その繰り返しが大切です。<br>
+        3年もすれば、一人前として活躍できるでしょう。
+	</p><br>
+	<p>
+        荒井設備の社員は、穏やかな人ばかり。
+		休憩中は、車の中で好きな音楽を聴いたり、外の景色を楽しんだり。そんなちょっと自由な空気も、私たちの仕事の魅力かもしれません。
+	</p>
+</section>
+<section class="content" id="numeric_data">
+	<h2>数字で見る当社</h2>
+	<p class="remark"></p>
+	<div class="large">
+		<p>売上高</p>
+		<p class="num"><span>2</span>億<span>805</span>万円</p>
+		<p class="year"></p>
+	</div>
+	<ul class="gr_con">
+		<li class="small">
+			<p>平均年齢</p>
+			<p class="num"><span>48</span>歳</p>
+		</li>
+		<li class="small">
+			<p>平均勤続年数</p>
+			<p class="num"><span>16</span>年</p>
+		</li>
+		<li class="small">
+			<p>平均残業時間</p>
+			<p class="num">月<span>3</span>時間</p>
+		</li>
+		<li class="small">
+			<p>年間休日</p>
+			<p class="num"><span>126</span>日</p>
+			<p class="at"></p>
+		</li>
+		<li class="small">
+			<p>設立年</p>
+			<p class="num"><span>1974</span>年</p>
+		</li>
+		<li class="small">
+			<p>平均有給取得日数</p>
+			<p class="num"><span>10</span>日</p>
+		</li>
+	</ul>
+	<ul class="fl_con">
+	<li>
+			<div class="chart">
+				<p>育児休暇率</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 45, [
+						[ 'value' => 100, 'caption' => '', 'color' => '#d9d9d9'],
+						[ 'value' => 0.01, 'caption' => '取得者無し', 'color' => '#2378C7'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+			<div class="chart">
+				<p>新卒・中途の比率</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 45, [
+						[ 'value' => 0.01, 'caption' => '', 'color' => '#2378C7'],
+						[ 'value' => 100, 'caption' => '中途100%', 'color' => '#F34444'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+			<div class="chart">
+				<p>男女比</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 40, [
+						[ 'value' => 70, 'caption' => '男性', 'color' => '#0062B5'],
+						[ 'value' => 30, 'caption' => '女性', 'color' => '#FF6E7E'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+		<div class="chart">
+			<p>年齢構成</p>
+			<svg width="150" height="150" viewBox="0 0 200 200">
+				<?php create_donut_graph(100, 100, 100, 40, [
+					[ 'value' => 0.01, 'caption' => "", 'color' => '#808080'],
+					[ 'value' => 10, 'caption' => '20代', 'color' => '#0073B7'],
+					[ 'value' => 10, 'caption' => '30代', 'color' => '#00AEEF'],
+					[ 'value' => 30, 'caption' => '40代', 'color' => '#00B398'],
+					[ 'value' => 30, 'caption' => '50代', 'color' => '#009F6B'],
+					[ 'value' => 20, 'caption' => '60代', 'color' => '#008C4A'],
+				]); ?>
+			</svg>
+		</div>
+		<li>
+			<div class="chart" >
+				<p>通勤手段の割合</p>
+				<svg width="150" height="150" viewBox="0 0 200 200">
+					<?php create_donut_graph(100, 100, 100, 40, [
+						[ 'value' => 100, 'caption' => "車・バイク100%", 'color' => '#E06A3A'],
+						[ 'value' => 0.001, 'caption' => '', 'color' => '#D04255'],
+					]); ?>
+				</svg>
+			</div>
+		</li>
+		<li>
+			<div class="chart">
+				<p>出身地の割合</p>
+					<svg width="150" height="150" viewBox="0 0 200 200">
+						<?php create_donut_graph(100, 100, 100, 40, [
+							[ 'value' => 100, 'caption' => "長野県内100%", 'color' => '#B2CF3E'],
+							[ 'value' => 0.01, 'caption' => '', 'color' => '#FEE929'],
+													]); ?>
+					</svg>
+				</div>
+		</li>
+	</ul>	
+</section>
+<section class="content" id="company_info">
+	<h2>会社概要</h2>
+	<img src="company.jpg" alt="" class="scene">
+	<dl class="speclist">
+		<div>
+			<dt>会社名</dt>
+			<dd><?= $co['company_name'] ?></dd>
+		</div>
+		<div>
+			<dt>業種</dt>
+			<dd><?= INDUSTRIES[$co['industry'][0]] ?></dd>
+		</div>
+		<div>
+			<dt>事業内容</dt>
+			<dd>
+				<ul class="li_style">
+					<li>管工事業</li>
+				</ul>
+			</dd>
+		</div>
+		<div>
+			<dt>代表者名</dt>
+			<dd>荒井浩正</dd>
+		</div>
+		<div>
+			<dt>従業員数</dt>
+			<dd>10名（男性 7名／女性 3名、役員、パート・アルバイト含む）</dd>
+		</div>
+		<div>
+			<dt>本社所在地</dt>
+			<dd>〒<?= $co['post_code']?><br><?= $co['address']?><br>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6425.202445926768!2d138.311116!3d36.37043700000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601dbbdc83a61157%3A0xd33688d7940dbeaf!2z5pel5pys44CB44CSMzg5LTA1MDUg6ZW36YeO55yM5p2x5b6h5biC5ZKM77yR77yY77yR77yY4oiS77yT!5e0!3m2!1sja!2sus!4v1751264781203!5m2!1sja!2sus" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</dd>
+		</div>
+		<div>
+			<dt>本社電話番号</dt>
+			<dd>0268-62-1615</dd>
+		</div>
+		<div>
+			<dt>ホームページ</dt>
+			<dd><a href="https://arai-facility621615.storeinfo.jp/" target="<?= $co['slug'] ?>">https://arai-facility621615.storeinfo.jp/g</a></dd>
+		</div>
+	</dl>
+</section>
+<section class="content" id="from_president">
+	<h2>会社からのメッセージ</h2>
+	<div class="pre_con">
+		<figure class="portrait">
+			<img src="arai.jpg" alt="">
+			<figcaption><span class="name">荒井浩正</span></figcaption>
+		</figure>
+		<p>
+			私たちの仕事は、地面の下や壁の裏――人の目には見えないところを整える仕事です。派手さはないけれど「なくてはならない」存在。人の暮らしを支える、大事な役割を担っています。
+		</p><br>
+		<p>
+            現場では、毎回違う状況や課題に向き合います。決まった正解があるわけじゃないからこそ、自分なりの工夫や判断が武器になります。<br>
+			型にはまらず、自由な感性を持っている人ほど、この仕事を楽しめると思います。
+		</p>
+		<p>
+			体を動かすのが好きな人、知らないことにワクワクできる人、そんな人を歓迎します！
+            少しでも「やってみたい」と思ったら、まずは現場を見てください。最初は何もわからなくて当然です。<br>一歩ずつ、手を動かしながら覚えていきましょう。
+		</p>
+	</div>
+</section>
+<div class="visit content">
+    <h2>応募前見学について</h2>
+	<dl class="speclist">
+    <?php foreach ($visit as $plan): ?>
+    <div class="visit-item">
+        <h3><?= $plan['about'] ?>について</h3>
+        <div>
+            <dt>開催日</dt>
+            <dd>
+				<ul>
+						<?php 
+						if (is_array($plan['date'])) {
+								foreach ($plan['date'] as $date) {
+										if (is_array($date)) { 
+											echo "<li>" . $date['name'] . "</li>";
+										}
+								}
+						} else {
+								echo "<li>" . $plan['date'] . "</li>";
+						}
+						?>
+				</ul>
+            </dd>
+        </div>
+        <div>
+            <dt>内容</dt>
+            <dd><?= $plan['content'] ?></dd>
+        </div>
+    </div>
+    <?php endforeach; ?>
+	</dl>
+	<a href="apply/#entry" class="flag_btn_entry">
+		<img src="<?= $highschool_base ?>flag.png" class="flag">
+		<span>
+			就活前に会社を見てみよう!<br>
+			応募前見学
+		</span>
+		<img src="<?= $highschool_base ?>arrow.svg">
+	</a>
+</div>
+
+</main>
+<?php include $highschool_base . 'tmpl_navi.php' ?>
+</div>
+<script src="../../tab.js"></script>
+<script src="../../page_nav.js"></script>
+<script src="<?= $highschool_base ?>lightbox.js"></script>
+<?php //include $base . '../footer.php' ?>
+</body>
