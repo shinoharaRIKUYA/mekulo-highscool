24259eed7cc6a42da8f6c8a3b43c23ba96c42279
sho <narita@nextreservation.jp>
2025-07-01 19:23:02 +0900

    応募するボタンを文章に

--- c/sakaguchi-seisakuzyo/apply/index.php
+++ c/sakaguchi-seisakuzyo/apply/index.php
@@ -91,11 +91,7 @@ $og_image = 'https://works.mekulo.jp/ogp-image.png';
 			<li>見学・体験<span>30分程度</span></li>
 		</ul>
 	</div>
-	<a href="../../../entry?company=<?= $co['slug'] ?>" class="entry-btn">
-		<img src="../../../flag.png" class="flag">
-		<span>応募する</span>
-		<img src="../../../arrow.svg">
-	</a>
+	<p class="entry_txt">応募前見学をご希望の方は、担任の先生や、進路指導の先生にご相談ください。</p>
 </section>
 </main>
 <?php include $highschool_base . 'tmpl_navi.php' ?>
